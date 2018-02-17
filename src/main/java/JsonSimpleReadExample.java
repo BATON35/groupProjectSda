4d50a1a73b7645ebcc418f6af67dbc0f2993bc7e
lukasz komorowski <komorozo87@gmail.com>
2018-02-17 14:55:36 +0100

    json update

--- src/main/java/JsonSimpleReadExample.java
+++ src/main/java/JsonSimpleReadExample.java
@@ -1,4 +1,3 @@
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -7,7 +6,9 @@ import org.json.simple.parser.ParseException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 
 public class JsonSimpleReadExample {
@@ -18,9 +19,23 @@ public class JsonSimpleReadExample {
         JSONParser parser = new JSONParser();
         try {
 
-            Object obj = parser.parse(new FileReader("C:\\Users\\Konrad\\Desktop\\projektGrupowy\\groupProjectSda\\listaNBP.json"));
-
+            Object obj = parser.parse(new FileReader("listaNBP.json"));
             System.out.println(obj);
+            JSONObject jsonObject = (JSONObject) obj;
+            String currency = (String) jsonObject.get("currency");
+            System.out.println(currency);
+
+            JSONArray tablicaWalut = (JSONArray) jsonObject.get("rates");
+            Iterator<Object> iterator = tablicaWalut.iterator();
+            List<Object> objectList = new ArrayList<Object>();
+            while (iterator.hasNext()) {
+                int index = 0;
+                objectList.add(index, iterator.next().toString());
+                index++;
+            }
+            for (Object object : objectList) {
+                System.out.println(object);
+            }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -29,6 +44,7 @@ public class JsonSimpleReadExample {
         } catch (ParseException e) {
             e.printStackTrace();
         }
+
 
     }
 
