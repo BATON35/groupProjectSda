40618dcfe7f75162f19539d3fa610b347145c469
lukasz komorowski <komorozo87@gmail.com>
2018-02-17 13:59:10 +0100

    interface update

--- src/main/java/Interface.java
+++ src/main/java/Interface.java
@@ -1,13 +1,9 @@
 public class Interface {
     public static void main(String[] args) {
-<<<<<<< HEAD
-=======
 
         User os1 = new User("lukas","abcd");
         Login.isVaildLogin(os1);
         AddUsers.addUser(os1);
-
-
->>>>>>> 9aeb2c4366265ca49e87661836bd153903ba5b67
+        
     }
 }
