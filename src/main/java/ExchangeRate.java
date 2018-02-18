fdb054dad886a9d2197730eba741f1463c278d04
lukasz komorowski <komorozo87@gmail.com>
2018-02-18 09:52:59 +0100

    retofit

---
+++ src/main/java/ExchangeRate.java
@@ -0,0 +1,15 @@
+import com.google.gson.annotations.SerializedName;
+
+public class ExchangeRate {
+
+
+    @SerializedName("no")
+    String no;
+
+    @SerializedName("effectiveDate")
+    String effectiveDate;
+
+    @SerializedName("mid")
+    Double mid;
+
+}
